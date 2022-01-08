4dda172c91400d6081dbbf5e4a53d2aa58179a06
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-08 20:16:48 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Band Name Generator Program
+
+
+print("Welcome to the Band Name Generator")
+
+city = input("Which City did you grow up in?\n")
+pet = input("What is the Name of Your PET?\n")
+
+print("Your Band Name Could Be:" + city + " " + pet)
